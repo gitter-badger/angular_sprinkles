2d2e7c8c22cb0420762235a96339931285ac6484
Gabe Scholz <gabe@brewhouse.io>
2014-10-03 16:36:05 -0700

    Remove white space :grin:

--- lib/angular_sprinkles/mixins/form_builder.rb
+++ lib/angular_sprinkles/mixins/form_builder.rb
@@ -3,7 +3,6 @@ module AngularSprinkles
     module FormBuilder
       def bind(helper, *args)
         params = { helper: helper, method: method(helper), args: args, object: @object }
-
         AngularSprinkles::FormBinder.new(params).call
       end
     end

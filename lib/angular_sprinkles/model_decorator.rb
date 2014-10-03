92f82f9ad685309605c54de89938525a6c1ff312
Gabe Scholz <gabe@brewhouse.io>
2014-10-03 10:37:17 -0700

    Delegate #class to the wrapped object when using #bindable

--- lib/angular_sprinkles/model_decorator.rb
+++ lib/angular_sprinkles/model_decorator.rb
@@ -4,13 +4,20 @@ module AngularSprinkles
       @key = args.fetch(:key)
       @json_wrapper = args.fetch(:json_wrapper)
       @object_wrapper = args.fetch(:object_wrapper)
+      @object = args.fetch(:object)
 
-      object = args.fetch(:object)
-      super(object)
+      super(@object)
     end
 
     def bind(attribute = nil)
       @object_wrapper.new(@key, attribute, @json_wrapper)
+    end
+
+    ##
+    # Rails uses #class as a part of #form_for to create labels.
+    # Delegate this method to the model class to prevent this issue.
+    def class
+      @object.class
     end
   end
 end

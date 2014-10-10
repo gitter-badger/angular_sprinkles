f3e1b5fa87f3f5811b6d2558bce184768bcd0271
Gabe Scholz <gabe@brewhouse.io>
2014-10-10 12:32:50 -0700

    Fix bug where block was being counted as an argument

--- lib/angular_sprinkles/form_binder/default.rb
+++ lib/angular_sprinkles/form_binder/default.rb
@@ -9,11 +9,15 @@ module AngularSprinkles
         @args.tap do |args|
           # Assume that optional params default to {}
           # This is a safe assumption for all helpers except check_box
-          (@method.parameters.count - args.count).times { args.push({}) }
+          (non_block_parameter_count - args.count).times { args.push({}) }
 
           # The last argument is always the one where ng-model should be appended
           args.last.reverse_merge!(binding)
         end
+      end
+
+      def non_block_parameter_count
+        @method.parameters.count { |parameter| parameter.first != :block }
       end
     end
   end

056f8477b68c8be983f50846bde50b18225d2956
Gabe Scholz <gabe@brewhouse.io>
2014-10-10 11:24:59 -0700

    Remove conditional in FormBinder.new with an extra helper method

--- lib/angular_sprinkles/mixins/form_builder.rb
+++ lib/angular_sprinkles/mixins/form_builder.rb
@@ -1,6 +1,6 @@
 module AngularSprinkles
   module Mixins
-    DEFAULT_HELPERS = ActionView::Helpers::FormBuilder.field_helpers - [:label, :fields_for]
+    DEFAULT_HELPERS = ActionView::Helpers::FormBuilder.field_helpers - [:label, :fields_for, :check_box]
     # DATE_HELPERS = [:date_select, :time_select, :datetime_select]
     # SELECT_HELPERS = [:select, :collection_select, :grouped_collection_select, :time_zone_select, :collection_check_boxes, :collection_radio_buttons]
     # ALL_HELPERS = [*DATE_HELPERS, *SELECT_HELPERS, *DEFAULT_HELPERS]
@@ -9,11 +9,17 @@ module AngularSprinkles
       DEFAULT_HELPERS.each do |helper|
         class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
           def bind_#{helper}(*args, &block)
-            params = { helper: :#{helper}, method: method(:#{helper}), args: args, object: @object }
-            args_with_binding = AngularSprinkles::FormBinder.new(params).call
+            params = { method: method(:#{helper}), args: args, object: @object }
+            args_with_binding = AngularSprinkles::FormBinder::Default.new(params).call
             #{helper}(*args_with_binding, &block)
           end
         RUBY_EVAL
+      end
+
+      def bind_check_box(*args, &block)
+        params = { method: method(:check_box), args: args, object: @object }
+        args_with_binding = AngularSprinkles::FormBinder::CheckBox.new(params).call
+        check_box(*args_with_binding, &block)
       end
     end
   end

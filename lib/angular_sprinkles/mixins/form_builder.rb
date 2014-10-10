0aa59d246cd859a5abe8c9b9807cb42cdf428143
Gabe Scholz <gabe@brewhouse.io>
2014-10-10 10:54:36 -0700

    Comment out additional helpers and add them to a TODO list

--- lib/angular_sprinkles/mixins/form_builder.rb
+++ lib/angular_sprinkles/mixins/form_builder.rb
@@ -1,12 +1,12 @@
 module AngularSprinkles
   module Mixins
-    DATE_HELPERS = [:date_select, :time_select, :datetime_select]
-    SELECT_HELPERS = [:select, :collection_select, :grouped_collection_select, :time_zone_select, :collection_check_boxes, :collection_radio_buttons]
     DEFAULT_HELPERS = ActionView::Helpers::FormBuilder.field_helpers - [:label, :fields_for]
-    ALL_HELPERS = [*DATE_HELPERS, *SELECT_HELPERS, *DEFAULT_HELPERS]
+    # DATE_HELPERS = [:date_select, :time_select, :datetime_select]
+    # SELECT_HELPERS = [:select, :collection_select, :grouped_collection_select, :time_zone_select, :collection_check_boxes, :collection_radio_buttons]
+    # ALL_HELPERS = [*DATE_HELPERS, *SELECT_HELPERS, *DEFAULT_HELPERS]
 
     module FormBuilder
-      ALL_HELPERS.each do |helper|
+      DEFAULT_HELPERS.each do |helper|
         class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
           def bind_#{helper}(*args, &block)
             params = { helper: :#{helper}, method: method(:#{helper}), args: args, object: @object }

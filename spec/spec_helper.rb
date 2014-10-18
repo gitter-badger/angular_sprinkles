01ebddd3d6a0b523aa7d248604a417c77bb16f18
Gabe Scholz <gabe@brewhouse.io>
2014-10-17 21:45:39 -0700

    Remove spec folder for Coveralls?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 if ENV["COVERALLS_REPO_TOKEN"]
   require "coveralls"
   Coveralls.wear! do
-    add_filter 'dummy'
-    add_filter 'rails_helper.rb'
-    add_filter 'spec_helper.rb'
+    add_filter 'spec'
   end
 end
 

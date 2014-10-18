898d5df7e5826bcd887a13bb54028186057cfc0f
Gabe Scholz <gabe@brewhouse.io>
2014-10-17 21:25:08 -0700

    Add cache to KeyGenerator

--- lib/angular_sprinkles/key_generator.rb
+++ lib/angular_sprinkles/key_generator.rb
@@ -2,13 +2,19 @@ module AngularSprinkles
   class KeyGenerator
     def initialize(args)
       @counter = args.fetch(:counter)
+      @cache = args.fetch(:cache)
     end
 
     def call(object)
+      return @cache.get(object) if @cache.set?(object)
+
       klass = "#{object.class}".gsub("::", "__")
       count = @counter.inc(klass)
+      generated_key = "#{klass}_#{count}"
 
-      "#{klass}_#{count}"
+      @cache.set(object, generated_key)
+
+      generated_key
     end
   end
 end
--- spec/angular_sprinkles/key_generator_spec.rb
+++ spec/angular_sprinkles/key_generator_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe AngularSprinkles::KeyGenerator do
-  let(:counter) { double(inc: inc) }
+  let(:counter) { AngularSprinkles::Counter.new }
+  let(:cache) { AngularSprinkles::Cache.new }
   let(:inc) { 1 }
   let(:object) { double(class: AngularSprinkles) }
 
-  subject { described_class.new(counter: counter) }
+  subject { described_class.new(counter: counter, cache: cache) }
 
   it "returns a suitable variable name" do
     expect(subject.call(object)).to eq("#{object.class}_#{inc}")
@@ -13,5 +14,10 @@ describe AngularSprinkles::KeyGenerator do
 
   it "it substitutes :: for __ in nested classes" do
     expect(subject.call(subject)).to eq("AngularSprinkles__KeyGenerator_#{inc}")
+  end
+
+  it "returns the same key if the object is used twice" do
+    expect(subject.call(object)).to eq("#{object.class}_#{inc}")
+    expect(subject.call(object)).to eq("#{object.class}_#{inc}")
   end
 end
